73eb039c2de509eb1a04172ccb825cbb4fbe8089
Phong Truong <phong@passioncorners.com>
2021-11-16 12:24:11 +0700

    flash message

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link :to="{ name: 'EventList' }">Events</router-link> |
-    <router-link :to="{ name: 'About' }">About</router-link>
+  <div id="app">
+    <div id="flashMessage" v-if="GStore.flashMessage">
+      {{ GStore.flashMessage }}
+    </div>
+    <div id="nav">
+      <router-link :to="{ name: 'EventList' }">Events</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link>
+    </div>
+    <router-view />
   </div>
-  <router-view />
 </template>
-
+<script>
+export default {
+  inject: ["GStore"],
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -13,6 +22,20 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+#flashMessage {
+  animation-name: yellowfade;
+  animation-duration: 3s;
 }
 
 #nav {
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -6,9 +6,17 @@
 <script>
 export default {
   props: ["event"],
+  inject: ["GStore"],
   methods: {
     register() {
       // call to api
+      // redirect
+      //flash message
+      this.GStore.flashMessage =
+        "You are successfully registered for" + this.event.title
+      setTimeout(() => {
+        this.GStore.flashMessage = ""
+      }, 3000)
       // redirect
       this.$router.push({
         name: "EventDetail",
