e75036edf9af542eaa2ae99303542a1ee6d354e8
Phong Truong <phong@passioncorners.com>
2021-11-16 11:38:44 +0700

    pro nav

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,10 +1,20 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
   props: ["event"],
+  methods: {
+    register() {
+      // call to api
+      // redirect
+      this.$router.push({
+        name: "EventDetail",
+      })
+    },
+  },
 }
 </script>
 
