de1a96ccf1f2202b39d69e56de0f7a4dff6165bc
Phong Truong <phong@passioncorners.com>
2021-11-16 13:06:37 +0700

    progress bar

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -27,7 +27,7 @@
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue"
 import EventSerivce from "@/services/EventService.js"
-import { watchEffect } from "vue"
+import NProgress from "nprogress"
 
 export default {
   name: "EventList",
@@ -41,18 +41,35 @@ export default {
       totalEvents: 0,
     }
   },
-  created() {
-    watchEffect(() => {
-      this.events = null
-      EventSerivce.getEvents(2, this.page)
-        .then((res) => {
-          this.events = res.data
-          this.totalEvents = res.headers["x-total-count"]
+  beforeRouteEnter(routeTo, RouteFrom, next) {
+    NProgress.start()
+    EventSerivce.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then((res) => {
+        next((comp) => {
+          comp.events = res.data
+          comp.totalEvents = res.headers["x-total-count"]
         })
-        .catch(() => {
-          this.$router.push({ name: "NetworkError" })
-        })
-    })
+      })
+      .catch(() => {
+        next({ name: "NetworkError" })
+      })
+      .finally(() => {
+        NProgress.done()
+      })
+  },
+  beforeRouteUpdate(routeTo) {
+    NProgress.start()
+    EventSerivce.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then((res) => {
+        this.events = res.data
+        this.totalEvents = res.headers["x-total-count"]
+      })
+      .catch(() => {
+        return { name: "NetworkError" }
+      })
+      .finally(() => {
+        NProgress.done()
+      })
   },
   computed: {
     hasNextPage() {
